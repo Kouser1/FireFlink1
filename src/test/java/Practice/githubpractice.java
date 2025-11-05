dd13afe2f641db902bdaeeac000a15c3da83c494
Sumaiya Kouser <Sumaiya Kouser@FFE-Sumaiya-Kouser.mshome.net>
2025-11-05 16:09:56 +0530

    second commit

--- src/test/java/Practice/githubpractice.java
+++
@@ -1,7 +0,0 @@
-package Practice;
-
-public class githubpractice {
-public static void main(String[] args) {
-	System.out.println("hi");
-}
-}
---
+++ src/test/java/Practiceforjenkins/jenkinspractice.java
@@ -0,0 +1,5 @@
+package Practiceforjenkins;
+
+public class jenkinspractice {
+
+}
